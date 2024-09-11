cc13122319ee7668fdd75786b2e1eee540808cd6
luq-mne <luqman.yazrie@outlook.com>
2024-09-12 07:16:54 +0800

    card component, basic home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import styles from './App.module.css';
+import banner from './assets/banner.png'
+import Card from './components/Card';
 
 function App() {
+  console.log("Hello World");
+  
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+    <div>
+      <header>
+        <h1>
+          Solid Merch
+        </h1>
       </header>
+
+      <img src={banner} alt="site banner"></img>
+
+      <Card />
     </div>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+export default function Card() {
+    return (
+        <div>
+            <h2>Card component</h2>
+            <p>
+                Greetings there, this is a card component that is reusable anywhere!
+            </p>
+            <button class="btn">Click me</button>
+        </div>
+    )
+}
