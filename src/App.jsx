889845cd14259c7965145f7f0f63365db05e7871
luq-mne <luqman.yazrie@outlook.com>
2024-09-12 10:46:09 +0800

    added tailwindcss, and my first prop on Cards

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,19 @@ function App() {
   
   return (
     <div>
-      <header>
+      <header class="cointainer m-auto">
         <h1>
           Solid Merch
         </h1>
       </header>
 
-      <img src={banner} alt="site banner"></img>
+      <img class= "rounded-md"src={banner} alt="site banner"></img>
 
-      <Card />
+      <div class= "grid grid-cols-3 gap-4 my-4">
+        <Card title= "Epic"/>
+        <Card title= "Wow this works?"/>
+        <Card />
+      </div>
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
-export default function Card() {
+export default function Card(props) {
     return (
-        <div>
-            <h2>Card component</h2>
+        <div class="bg-white p-4  text-center rounded-md shadow-ms">
+            <h2>{props.title}</h2>
             <p>
                 Greetings there, this is a card component that is reusable anywhere!
             </p>
