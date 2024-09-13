ef14ffbed5b76dcd614569a9042022a932dbbeea
opo <169742991+oponic@users.noreply.github.com>
2024-09-12 20:11:04 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,11 +26,12 @@ function App(props) {
         </span>
 
         <h1>
-          Solid Merch
+          Solitude
         </h1>
 
         <A href="/">Home</A>
         <A href="/cart">Cart</A>
+        <A href="/Solidarity">About Us</A>
       </header>
 
       <img class= "rounded-md"src={banner} alt="site banner"></img>
